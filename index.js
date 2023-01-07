8b658ddc2f600a30ee63977206bda106f29abda3
Nathan Eloe <powerofazure@gmail.com>
2023-01-07 12:20:41 -0600

    Create view with org avatar

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 require('dotenv').config()
-const { Octokit } = require("@octokit/rest");
+const { Octokit } = require("@octokit/rest")
+const http = require('http')
+const dots = require('dot').process({path: "./views"})
+const port = process.env.PORT || 8000
+const host = process.env.HOST || 'localhost'
 
 const octokit = new Octokit({auth:process.env.ACCESS_TOKEN})
-console.log(octokit.rest.orgs.createInvitation('/'+process.env.ORG_NAME, {invitee_id:'neloe'}).then((e)=>{console.log(e)}))
+
+function listener(req, res) {
+    console.log(req.url)
+    if (req.url === '/' && req.method === 'GET')
+    {
+        octokit.rest.orgs.get({ org:process.env.ORG_NAME}).then(e=>{
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(dots.index({avatar: e.data.avatar_url}))
+        })
+    }
+    
+}
+
+const server = http.createServer(listener)
+server.listen(port, host, () => {
+    console.log(`server running on http://${host}:${port}`)
+})
