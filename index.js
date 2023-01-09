5726bc98c25740078f899ba853a9dafa3d246ebb
Nathan Eloe <powerofazure@gmail.com>
2023-01-09 07:51:39 -0600

    Create submit field and handler; need POST body

--- index.js
+++ index.js
@@ -9,13 +9,19 @@ const octokit = new Octokit({auth:process.env.ACCESS_TOKEN})
 
 function listener(req, res) {
     console.log(req.url)
+    console.log(req.method)
     if (req.url === '/' && req.method === 'GET')
     {
         octokit.rest.orgs.get({ org:process.env.ORG_NAME}).then(e=>{
             res.setHeader("Content-Type", "text/html");
             res.writeHead(200);
-            res.end(dots.index({avatar: e.data.avatar_url}))
+            res.end(dots.index({avatar: e.data.avatar_url, org:process.env.ORG_NAME}))
         })
+    }
+    if (req.url === '/invite' && req.method === 'POST')
+    {
+        console.log(req.body)
+        res.end("got thing")
     }
     
 }
